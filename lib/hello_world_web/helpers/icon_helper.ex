30f2ffffc8b7510a78352ba07b4cb839a1814730
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-06-27 11:20:15 -0300

    SVG icons as images

---
+++ lib/hello_world_web/helpers/icon_helper.ex
@@ -0,0 +1,15 @@
+defmodule HelloWorldWeb.Helpers.IconHelper do
+  @moduledoc """
+  Give some icons to be used on templates
+  """
+  use Phoenix.HTML
+  alias HelloWorldWeb.Router.Helpers, as: Routes
+
+  def icon_tag(conn, name, opts \\ []) do
+    classes = Keyword.get(opts, :class, "") <> "icon"
+
+    content_tag(:svg, class: classes) do
+      tag(:use, "xlink:href": Routes.static_path(conn, "/images/icons.svg#" <> name))
+    end
+  end
+end
